d096db19e0e2fd2ba09af3346ad6194a6314be09
jaeheela <jhla456@kakao.com>
2023-04-19 03:34:23 +0900

    no message

--- core/src/main/java/hello/core/member/Grade.java
+++
@@ -1,7 +0,0 @@
-package hello.core.member;
-
-//회원 도메인 개발 - 회원 등급
-public enum Grade {
-    BASIC,
-    VIP
-}
--- core/src/main/java/hello/core/member/MemberService.java
+++
@@ -1,7 +0,0 @@
-package hello.core.member;
-
-//회원 서비스 - 회원 서비스 인테페이스
-public interface MemberService {
-    void join(Member member);
-    Member findMember(Long memberId);
-}
