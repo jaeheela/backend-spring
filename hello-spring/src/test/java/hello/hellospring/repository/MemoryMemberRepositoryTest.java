d95c29de40674b6e47786986b2b277968994dbb2
jaeheela <jhla456@kakao.com>
2025-07-11 15:49:29 +0900

    [Clean] MemberMemberRepositoryTest 불필요한 주석 제거

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -8,28 +8,11 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-//회원 리포지토리 메모리 구현체 테스트
 
-//회원 리포지토리 테스트 케이스 작성
-// => 개발한 기능을 실행해서 테스트 할 때 자바의 main 메서드를 통해서 실행하거나, 웹 애플리케이션의 컨트롤러를 통해서 해당 기능을 실행한다.
-// => 이러한 방법은 준비하고 실행하는데 오래 걸리고, 반복 실행하기 어렵고 여러 테스트를 한번에 실행하기 어렵다는 단점이 있다.
-// => 자바는 JUnit이라는 프레임워크로 테스트를 실행해서 이러한 문제를 해결한다.
-
-//테스트 케이스 작성 팁
-// => 작품을 만들기 전에 검증할 수 있는 틀을 먼저 만들 수도 있음 - 테스트주도개발(TDD)
-// => 테스트가 한개면 모르지만 수백개라면? - 나중에 빌드하거나, gradlew 띄우고 테스트하면 테스트를 자동으로 돌려줌
 class MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository = new MemoryMemberRepository();
 
-    //테스트 메소드 하나 끝날때마다 반복적으로 호출될 메소드 - 콜백메소드
-    // => 테스트 메소드가 끝날때마다 레파지토리를 지워줌 - 테스트 메소드의 순서가 상관없어짐
-    //@AfterEach 어노테이션
-    // => 한번에 여러 테스트를 실행하면 메모리 DB에 직전 테스트의 결과가 남을 수 있다.
-    // => 이렇게 되면 다음 이전 테스트 때문에 다음 테스트가 실패할 가능성이 있다.
-    // => @AfterEach 를 사용하면 각 테스트가 종료될 때 마다 이 기능을 실행한다.
-    // => 여기서는 메모리 DB에 저장된 데이터를 삭제한다.
-    // => 테스트는 각각 독립적으로 실행되어야 한다. 테스트 순서에 의존관계가 있는 것은 좋은 테스트가 아니다.
     @AfterEach
     public void afterEach(){
         repository.clearStore();
@@ -37,25 +20,11 @@ class MemoryMemberRepositoryTest {
 
     @Test
     public void save(){
-        //given (상황)
         Member member = new Member();
         member.setName("spring");
-
-        //when (행동)
         repository.save(member);
-
-        //then (결과)
         Member result=repository.findById(member.getId()).get();
-        //System.out.println("result = "+(result==member)); //result = true
-
-        //가져온 결과값[result]이 [member]와 같나요??
-        //Assertions.assertEquals(member,result); //같으면 초록불 들어옴
-        //Assertions.assertEquals(member,null); //다르면 빨간불 들어옴
-        //Expected :hello.hellospring.domain.Member@77f1baf5 - 기대한건 이건데
-        //Actual   :null - 실제값은 null이 들어왔어!
-
-        //Assertions.assertThat(result).isEqualTo(null);
-        assertThat(result).isEqualTo(member); //option + enter : static import
+        assertThat(result).isEqualTo(member);
     }
 
     @Test
@@ -64,7 +33,6 @@ class MemoryMemberRepositoryTest {
         Member member1=new Member();
         member1.setName("spring1");
         repository.save(member1);
-
         Member member2=new Member();
         member2.setName("spring2");
         repository.save(member2);
@@ -74,7 +42,7 @@ class MemoryMemberRepositoryTest {
 
         //then (결과)
         assertThat(result).isEqualTo(member1);
-        //Assertions.assertThat(result).isEqualTo(member2);
+//        assertThat(result).isEqualTo(member2);
 
     }
 
@@ -84,16 +52,16 @@ class MemoryMemberRepositoryTest {
         Member member1=new Member();
         member1.setName("spring1");
         repository.save(member1);
-
         Member member2=new Member();
         member2.setName("spring2");
         repository.save(member2);
+
         //when (행동)
         List<Member> result= repository.findAll();
 
         //then (결과)
         assertThat(result.size()).isEqualTo(2);
-        //Assertions.assertThat(result.size()).isEqualTo(3);
+//        Assertions.assertThat(result.size()).isEqualTo(3);
     }
 
 }
