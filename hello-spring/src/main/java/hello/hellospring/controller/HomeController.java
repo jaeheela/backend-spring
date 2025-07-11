c3699e08c91aa087005577d3c920e7dfe64ed439
[3Softplus] 라재희 <jaeheela@3softplus.com>
2025-07-11 14:03:48 +0900

    [clean] 불필요한 주석 제거

--- hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
@@ -3,7 +3,6 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// => 참고로 컨트롤러가 정적 파일보다 우선순위가 높다.
 @Controller
 public class HomeController {
 
