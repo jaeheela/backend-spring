15192d5c90301059f087869faf26ff9f53f274b6
jaeheela <jhla456@kakao.com>
2025-07-11 16:33:34 +0900

    [Clean] MemberService, MemberServiceTest 불필요한 주석 제거

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,6 +1,7 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
